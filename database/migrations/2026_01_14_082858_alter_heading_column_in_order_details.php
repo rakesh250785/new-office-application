0a04f86670ddf8d63ab60f499fcc897156d9c047
Yadav <mkyadav59@gmail.com>
2026-01-14 14:08:46 +0530

    handle feature

---
+++ database/migrations/2026_01_14_082858_alter_heading_column_in_order_details.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('order_details', function (Blueprint $table) {
+            $table->longText('heading')->nullable()->change();
+        });
+
+        Schema::table('quotation_details', function (Blueprint $table) {
+            $table->longText('heading')->nullable()->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('order_details', function (Blueprint $table) {
+            $table->string('heading', 255)->nullable()->change();
+        });
+
+        Schema::table('quotation_details', function (Blueprint $table) {
+            $table->string('heading', 255)->nullable()->change();
+        });
+    }
+};
